d5f5f0a7080609902b62caa1e78fac6d6b058da3
Jeongkyu Shin <inureyes@gmail.com>
2017-04-12 20:28:38 +0900

    * Add sample sqlite3 code to api route endpoint  * SQLite3 샘플 동작 코드를 추가 (express 홈페이지 코드)  * http://localhost:3000/api/ 로 접근하면 터미널에 데이터베이스 입출력이  실행되는 것을 확인할 수 있음.

--- note-app/app.js
+++ note-app/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var rest_api = require('./routes/rest_api')
 
 app.use('/', express.static(__dirname + '/'));
 
 app.all('/api', function (req, res, next) {
   console.log('API is called...');
-  next();
+  api = new rest_api();
+  api.run();
 });
 
 app.listen(3000, function () {
