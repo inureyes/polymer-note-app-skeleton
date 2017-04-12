57d121bf040a189fcd9d12e5f049358b4bf34c33
Jeongkyu Shin <inureyes@gmail.com>
2017-04-12 21:41:34 +0900

    refs #1 : modify - check whether table exists or not  * 테이블이 존재하는지 체크해서 존재할 경우 새로 생성하지 않도록 하는  코드 추가

--- note-app/routes/rest_api.js
+++ note-app/routes/rest_api.js
@@ -4,8 +4,19 @@ var db = new sqlite3.Database(':memory:')
 class rest_api {
   run() {
     db.serialize(function () {
-      db.run('CREATE TABLE lorem (info TEXT)')
-      var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+      db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='note'", function(error, row) {
+      		if (row !== undefined) {
+      			console.log("table exists. skipping creation");
+      		}	else {
+      			console.log("creating table")
+      			db.run("CREATE TABLE note (content TEXT)", function(error) {
+      				if (error.message.indexOf("already exists") != -1) {
+      					console.log(error);
+      				}
+      			});
+      		}
+      	});
+      var stmt = db.prepare('INSERT INTO note VALUES (?)')
 
       for (var i = 0; i < 10; i++) {
         stmt.run('Ipsum ' + i)
@@ -13,7 +24,7 @@ class rest_api {
 
       stmt.finalize()
 
-      db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+      db.each('SELECT rowid AS id, content FROM note', function (err, row) {
         console.log(row.id + ': ' + row.info)
       })
 
