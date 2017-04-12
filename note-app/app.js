69bf68f42834771a3f945f7c72d28927b9442c2c
Jeongkyu Shin <inureyes@gmail.com>
2017-04-12 19:40:53 +0900

    * Add API url for express server  * Add form polymer package  * API 주소를 처리할 수 있는 라우터 추가  * form 으로 데이터를 처리해주는 iron-form 추가

--- note-app/app.js
+++ note-app/app.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/'));
 
+app.all('/api', function (req, res, next) {
+  console.log('API is called...');
+  next();
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
